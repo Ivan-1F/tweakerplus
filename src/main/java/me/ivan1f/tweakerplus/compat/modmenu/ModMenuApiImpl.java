5d928679e54f1c6877c4157ea7f9f674bb0184e1
Ivan1F <wyf0762@foxmail.com>
2022-03-05 16:05:30 +0800

    1.18

--- src/main/java/me/ivan1f/tweakerplus/compat/modmenu/ModMenuApiImpl.java
+++ src/main/java/me/ivan1f/tweakerplus/compat/modmenu/ModMenuApiImpl.java
@@ -1,7 +1,7 @@
 package me.ivan1f.tweakerplus.compat.modmenu;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.ivan1f.tweakerplus.gui.TweakerPlusConfigGui;
 
 public class ModMenuApiImpl implements ModMenuApi {
