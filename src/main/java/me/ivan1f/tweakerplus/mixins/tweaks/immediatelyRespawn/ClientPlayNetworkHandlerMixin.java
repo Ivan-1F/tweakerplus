c5e956a18ffa87d9e657c81dbf724d15cd71b6b4
Ivan1F <wyf0762@foxmail.com>
2022-05-15 22:18:24 +0800

    1.17

--- src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/immediatelyRespawn/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/immediatelyRespawn/ClientPlayNetworkHandlerMixin.java
@@ -3,7 +3,8 @@ package me.ivan1f.tweakerplus.mixins.tweaks.immediatelyRespawn;
 import me.ivan1f.tweakerplus.config.TweakerPlusConfigs;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.s2c.play.CombatEventS2CPacket;
+import net.minecraft.network.packet.s2c.play.DeathMessageS2CPacket;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,17 +13,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-    @Shadow private MinecraftClient client;
+    @Final
+    @Shadow
+    private MinecraftClient client;
 
     @Inject(
-            method = "onCombatEvent",
+            method = "onDeathMessage",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/MinecraftClient;openScreen(Lnet/minecraft/client/gui/screen/Screen;)V"
+                    target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"
             ),
             cancellable = true
     )
-    private void immediatelyRespawn(CombatEventS2CPacket packet, CallbackInfo ci) {
+    private void immediatelyRespawn(DeathMessageS2CPacket packet, CallbackInfo ci) {
         if (TweakerPlusConfigs.IMMEDIATELY_RESPAWN.getBooleanValue()) {
             if (this.client.player != null) {
                 this.client.player.requestRespawn();
