0ff847f29229a7fbe0df41eac7d9fd526aad343a
Ivan1F <wyf0762@foxmail.com>
2022-08-08 12:50:37 +0800

    1.16

--- src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/enchantmentBookHint/ItemRendererMixin.java
+++ src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/enchantmentBookHint/ItemRendererMixin.java
@@ -39,14 +39,14 @@ public class ItemRendererMixin {
         matrixStack.scale(0.5F, 0.5F, 0.5F);
         VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
 
-        Set<Map.Entry<Enchantment, Integer>> entries = EnchantmentHelper.getEnchantments(stack).entrySet();
+        Set<Map.Entry<Enchantment, Integer>> entries = EnchantmentHelper.get(stack).entrySet();
         int currentY = y + 17 - entries.size() * 9 / 2;
 
         for (Map.Entry<Enchantment, Integer> entry : entries) {
             String string = entry.getKey().getName(entry.getValue()).getString();
             fontRenderer.draw(
                     string,
-                    (float) (x + 17 - fontRenderer.getStringWidth(string) / 2),
+                    (float) (x + 17 - fontRenderer.getWidth(string) / 2),
                     (float) (currentY),
                     16777215,
                     true,
