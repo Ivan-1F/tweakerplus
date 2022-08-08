50fbd4b440f83729ec8ad36b7ca1a961d14ffc98
Ivan1F <wyf0762@foxmail.com>
2022-08-08 12:58:03 +0800

    1.14

--- src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/enchantmentBookHint/ItemRendererMixin.java
+++ src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/enchantmentBookHint/ItemRendererMixin.java
@@ -1,11 +1,9 @@
 package me.ivan1f.tweakerplus.mixins.tweaks.enchantmentBookHint;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import me.ivan1f.tweakerplus.config.TweakerPlusConfigs;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.EnchantedBookItem;
@@ -34,30 +32,30 @@ public class ItemRendererMixin {
 
         Item item = stack.getItem();
         if (!(item instanceof EnchantedBookItem)) return;
-        MatrixStack matrixStack = new MatrixStack();
-        matrixStack.translate(x / 2.0, y / 2.0, (this.zOffset + 200.0F));
-        matrixStack.scale(0.5F, 0.5F, 0.5F);
-        VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+        GlStateManager.translated(x / 2.0, y / 2.0, (this.zOffset + 200.0F));
+        GlStateManager.scalef(0.5F, 0.5F, 0.5F);
 
         Set<Map.Entry<Enchantment, Integer>> entries = EnchantmentHelper.getEnchantments(stack).entrySet();
         int currentY = y + 17 - entries.size() * 9 / 2;
 
+        GlStateManager.disableLighting();
+        GlStateManager.disableDepthTest();
+        GlStateManager.disableBlend();
         for (Map.Entry<Enchantment, Integer> entry : entries) {
             String string = entry.getKey().getName(entry.getValue()).getString();
-            fontRenderer.draw(
+            fontRenderer.drawWithShadow(
                     string,
                     (float) (x + 17 - fontRenderer.getStringWidth(string) / 2),
                     (float) (currentY),
-                    16777215,
-                    true,
-                    matrixStack.peek().getModel(),
-                    immediate,
-                    false,
-                    0,
-                    15728880
+                    16777215
             );
+            
             currentY += 9;
         }
-        immediate.draw();
+        GlStateManager.scalef(2, 2, 2);
+        GlStateManager.translated(-x / 2.0, -y / 2.0, -(this.zOffset + 200.0F));
+        GlStateManager.enableBlend();
+        GlStateManager.enableLighting();
+        GlStateManager.enableDepthTest();
     }
 }
