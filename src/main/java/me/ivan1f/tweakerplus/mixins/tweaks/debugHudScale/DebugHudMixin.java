a44e415fcd66663294057d19ca5a486214aba02c
Ivan1F <wyf0762@foxmail.com>
2022-09-16 15:08:47 +0800

    1.17

--- src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/debugHudScale/DebugHudMixin.java
+++ src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/debugHudScale/DebugHudMixin.java
@@ -4,6 +4,7 @@ import me.ivan1f.tweakerplus.config.TweakerPlusConfigs;
 import me.ivan1f.tweakerplus.util.RenderUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.DebugHud;
+import net.minecraft.client.util.math.MatrixStack;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,32 +20,32 @@ public class DebugHudMixin {
     private RenderUtil.Scaler scaler = null;
 
     @Inject(method = "renderLeftText", at = @At("HEAD"))
-    private void tweakerPlus_debugHudScale_left_push(CallbackInfo ci) {
+    private void tweakerPlus_debugHudScale_left_push(MatrixStack matrices, CallbackInfo ci) {
         this.scaler = null;
         if (TweakerPlusConfigs.DEBUG_HUD_SCALE.isModified()) {
             this.scaler = RenderUtil.createScaler(0, 0, TweakerPlusConfigs.DEBUG_HUD_SCALE.getDoubleValue());
-            this.scaler.apply();
+            this.scaler.apply(matrices);
         }
     }
 
     @Inject(method = "renderLeftText", at = @At("RETURN"))
-    private void tweakerPlus_debugHudScale_left_pop(CallbackInfo ci) {
+    private void tweakerPlus_debugHudScale_left_pop(MatrixStack matrices, CallbackInfo ci) {
         if (this.scaler != null) {
             this.scaler.restore();
         }
     }
 
     @Inject(method = "renderRightText", at = @At("HEAD"))
-    private void tweakerPlus_debugHudScale_right_push(CallbackInfo ci) {
+    private void tweakerPlus_debugHudScale_right_push(MatrixStack matrices, CallbackInfo ci) {
         this.scaler = null;
         if (TweakerPlusConfigs.DEBUG_HUD_SCALE.isModified()) {
             this.scaler = RenderUtil.createScaler(this.client.getWindow().getScaledWidth(), 0, TweakerPlusConfigs.DEBUG_HUD_SCALE.getDoubleValue());
-            this.scaler.apply();
+            this.scaler.apply(matrices);
         }
     }
 
     @Inject(method = "renderRightText", at = @At("RETURN"))
-    private void tweakerPlus_debugHudScale_right_pop(CallbackInfo ci) {
+    private void tweakerPlus_debugHudScale_right_pop(MatrixStack matrices, CallbackInfo ci) {
         if (this.scaler != null) {
             this.scaler.restore();
         }
--- src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/subtitlesScale/SubtitlesHudMixin.java
+++ src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/subtitlesScale/SubtitlesHudMixin.java
@@ -4,6 +4,7 @@ import me.ivan1f.tweakerplus.config.TweakerPlusConfigs;
 import me.ivan1f.tweakerplus.util.RenderUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.SubtitlesHud;
+import net.minecraft.client.util.math.MatrixStack;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,16 +20,16 @@ public class SubtitlesHudMixin {
     private RenderUtil.Scaler scaler = null;
 
     @Inject(method = "render", at = @At("HEAD"))
-    private void tweakerPlus_subtitlesScale_push(CallbackInfo ci) {
+    private void tweakerPlus_subtitlesScale_push(MatrixStack matrices, CallbackInfo ci) {
         this.scaler = null;
         if (TweakerPlusConfigs.SUBTITLES_SCALE.isModified()) {
             this.scaler = RenderUtil.createScaler(this.client.getWindow().getScaledWidth(), this.client.getWindow().getScaledHeight() - 30, TweakerPlusConfigs.SUBTITLES_SCALE.getDoubleValue());
-            this.scaler.apply();
+            this.scaler.apply(matrices);
         }
     }
 
     @Inject(method = "render", at = @At("RETURN"))
-    private void tweakerPlus_subtitlesScale_pop(CallbackInfo ci) {
+    private void tweakerPlus_subtitlesScale_pop(MatrixStack matrices, CallbackInfo ci) {
         if (this.scaler != null) {
             this.scaler.restore();
         }
