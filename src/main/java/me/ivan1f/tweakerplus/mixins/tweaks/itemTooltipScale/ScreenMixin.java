f0243794c764f294ec9b9212df9ce40aeb1f62b6
Ivan1F <wyf0762@foxmail.com>
2022-11-19 22:18:07 +0800

    1.16

--- src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/itemTooltipScale/ScreenMixin.java
+++ src/main/java/me/ivan1f/tweakerplus/mixins/tweaks/itemTooltipScale/ScreenMixin.java
@@ -3,6 +3,8 @@ package me.ivan1f.tweakerplus.mixins.tweaks.itemTooltipScale;
 import me.ivan1f.tweakerplus.config.TweakerPlusConfigs;
 import me.ivan1f.tweakerplus.util.RenderUtil;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.OrderedText;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,8 +18,8 @@ public class ScreenMixin {
     @Nullable
     private RenderUtil.Scaler scaler = null;
 
-    @Inject(method = "renderTooltip(Ljava/util/List;II)V", at = @At("HEAD"))
-    private void tweakerPlus_itemTooltipScale_push(List<String> text, int x, int y, CallbackInfo ci) {
+    @Inject(method = "renderOrderedTooltip", at = @At("HEAD"))
+    private void tweakerPlus_itemTooltipScale_push(MatrixStack matrices, List<? extends OrderedText> lines, int x, int y, CallbackInfo ci) {
         this.scaler = null;
         if (TweakerPlusConfigs.ITEM_TOOLTIP_SCALE.isModified()) {
             this.scaler = RenderUtil.createScaler(x, y, TweakerPlusConfigs.ITEM_TOOLTIP_SCALE.getDoubleValue());
@@ -25,7 +27,7 @@ public class ScreenMixin {
         }
     }
 
-    @Inject(method = "renderTooltip(Ljava/util/List;II)V", at = @At("RETURN"))
+    @Inject(method = "renderOrderedTooltip", at = @At("RETURN"))
     private void tweakerPlus_itemTooltipScale_pop(CallbackInfo ci) {
         if (this.scaler != null) {
             this.scaler.restore();
