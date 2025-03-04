e71549bfd0535e9e982ff3ab57c58234d5433bbb
Ivan1F <wyf0762@foxmail.com>
2025-03-04 22:24:04 +0900

    better modmenu impl

--- src/main/java/me/ivan1f/tweakerplus/compat/modmenu/ModMenuApiImpl.java
+++ src/main/java/me/ivan1f/tweakerplus/compat/modmenu/ModMenuApiImpl.java
@@ -1,17 +1,36 @@
 package me.ivan1f.tweakerplus.compat.modmenu;
 
-//#if MC >= 11800
+//#if MC >= 11600
 //$$ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 //$$ import com.terraformersmc.modmenu.api.ModMenuApi;
-//#else
+//#elseif MC >= 11500
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
+//#else
+//$$ import io.github.prospector.modmenu.api.ModMenuApi;
+//$$ import me.ivan1f.tweakerplus.TweakerPlusMod;
+//$$ import net.minecraft.client.gui.screen.Screen;
+//$$ import java.util.function.Function;
 //#endif
+
 import me.ivan1f.tweakerplus.gui.TweakerPlusConfigGui;
 
+//#if MC < 11500
+//$$ @SuppressWarnings("deprecation")
+//#endif
 public class ModMenuApiImpl implements ModMenuApi {
+    //#if MC < 11500
+    //$$ public String getModId() {
+    //$$ 	return TweakerPlusMod.MOD_ID;
+    //$$ }
+    //#endif
+
     @Override
+    //#if MC >= 11500
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    //#else
+    //$$ public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+    //#endif
         return (screen) -> {
             TweakerPlusConfigGui gui = new TweakerPlusConfigGui();
             gui.setParent(screen);
